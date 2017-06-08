8d0a5dc0eb5cd3b26212e01a27420c8125efe780
shengjk1 <jsjsjs1789@163.com>
2017-06-08 23:17:54 +0800

    111111111111111

--- play/play22.py
+++ play/play22.py
@@ -9,6 +9,9 @@
 import urllib,urllib2,gevent
 #引入正则表达式模块，时间模块
 import re,time
+
+import requests
+from bs4 import BeautifulSoup
 from gevent import monkey
 
 monkey.patch_all()
@@ -20,7 +23,7 @@ def geturllist(url):
 	text = s.read()
 	#正则匹配，匹配其中的图片
 	html = re.search(r'<ol.*</ol>', text, re.S)
-	urls = re.finditer(r'<p><img src="(.+?)jpg" /></p>',html.group(),re.I)
+	urls = re.finditer(r'<p><img src="(...)jpg" /></p>',html.group(),re.I)
 	for i in urls:
 		url=i.group(1).strip()+str("jpg")
 		url_list.append(url)
@@ -29,7 +32,7 @@ def geturllist(url):
 def download(down_url):
 	name=str(time.time())[:-3]+"_"+re.sub('.+?/','',down_url)
 	print name
-	urllib.urlretrieve(down_url, "D:\\TEMP\\"+name)
+	urllib.urlretrieve(down_url, "D:\\tmp\\"+name)
 
 def getpageurl():
 	page_list = []
@@ -42,9 +45,20 @@ def getpageurl():
 	return page_list
 if __name__ == '__main__':
 	jobs = []
-	pageurl = getpageurl()[::-1]
-	#进行图片下载
-	for i in pageurl:
-		for (downurl) in geturllist(i):
-			jobs.append(gevent.spawn(download, downurl))
-	gevent.joinall(jobs)
+	# pageurl = getpageurl()[::-1]
+	# 进行图片下载
+	# for i in pageurl:
+	# 	for downurl in geturllist(i):
+	# 		jobs.append(gevent.spawn(download(downurl)))
+	#
+	# gevent.joinall(jobs)
+	import  requests
+	r=requests.get('http://jandan.net/ooxx');
+	soup=BeautifulSoup(r.content,'html.parser')
+	for i in soup.find_all(class_="view_img_link"):
+		print i['href'].encode("utf-8")
+		r=requests.get('http:'+i['href'].encode("utf-8"))
+		picName="D:\\tmp\\"+str(time.time())+".jpg"
+		with open(picName,"wb") as f:
+			f.write(r.content)
+
